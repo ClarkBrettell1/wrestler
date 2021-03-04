6b31a87b1ff63aa34e0007d247bcf058bb1742cc
clarkbrettell1 <clarkbrettell@gmail.com>
2021-03-04 17:29:30 +0000

    Propeller ad

--- index.php
+++ index.php
@@ -70,6 +70,7 @@
     <script src="assets/js/main.js">
 
     </script>
+    <script>(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://iclickcdn.com/tag.min.js',3996684,document.body||document.documentElement)</script>
     &#169; Copyright Clark Brettell 2021
   </footer>
 </body>
