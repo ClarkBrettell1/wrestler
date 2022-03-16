ee5caec2a5e0a50d14f2d90cee21e4333c508a1c
ClarkBrettell1 <45184224+ClarkBrettell1@users.noreply.github.com>
2022-03-16 08:08:10 +0000

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="Random Wrestler Name Generator at wrestlername.com can be used to generate your own wrestler name. Try it out, it's free.">
+  <meta name="description" content="Random Wrestler Name Generator at wrestlername.com">
   <title>Random Wrestler Generator | Wrestlername.com</title>
   <link rel="stylesheet" href="assets/css/main.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
